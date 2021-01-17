a394c871ac4d99f92dc271e744d8d1a3277de881
Wei Ding <ding_web@yahoo.co.jp>
2021-01-17 00:08:12 -0500

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SubPageComponent } from './sub-page/sub-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubPageComponent
   ],
   imports: [
     BrowserModule
